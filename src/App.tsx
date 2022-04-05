7587e07349df262bcc6a488dfb09380d4d5ca80c
Robert Sammataro <robbysam@udel.edu>
2022-04-05 16:26:17 -0400

    Added Robby's Name

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,8 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
             <p> William Hart </p>
+            <p> Robby Sammataro </p>
         </div>
     );
 }
