f3222b2fa364e8d7c5e5ea7a63f00224142c3bd0
WillHart2002 <wilhart@udel.edu>
2022-05-10 13:45:35 -0400

    added intial plans data in before each tests

--- src/tests/PlanViewer.test.tsx
+++ src/tests/PlanViewer.test.tsx
@@ -5,7 +5,47 @@ import { PlanViewer } from "../components/PlanViewer";
 
 describe("Planner Tests", () => {
     beforeEach(() => {
+        //making the initial data be initial plan
         render(<PlanViewer />);
+        //adding Spring 2022
+        const addSem = screen.getByTestId("addSem-button");
+        addSem.click();
+        const termSelect = screen.getByTestId("term-dropdown");
+        userEvent.selectOptions(termSelect, "Spring");
+        const inputBox = screen.getByTestId("year-textbox");
+        userEvent.type(inputBox, "2022");
+        const insSem = screen.getByTestId("insertSem-button");
+        insSem.click();
+        //setting course pool to add courses to Spring 2022
+        const termSelect2 = screen.getByTestId("course-pool-term-dropdown");
+        userEvent.selectOptions(termSelect2, "Spring");
+        const inputBox2 = screen.getByTestId("course-pool-entry-box");
+        const addCourse = screen.getByTestId("addCourse-pool-button");
+        //adding courses to Spring 2022
+        userEvent.type(inputBox2, "ENGL110");
+        addCourse.click();
+        userEvent.clear(inputBox2);
+        userEvent.type(inputBox2, "MATH210");
+        addCourse.click();
+        userEvent.clear(inputBox2);
+        userEvent.type(inputBox2, "CISC108");
+        addCourse.click();
+        userEvent.clear(inputBox2);
+        userEvent.type(inputBox2, "CISC181");
+        addCourse.click();
+        userEvent.clear(inputBox2);
+        //now adding a fall 2022 semester
+        addSem.click();
+        const termSelect3 = screen.getByTestId("term-dropdown");
+        userEvent.selectOptions(termSelect3, "Fall");
+        const insSem2 = screen.getByTestId("insertSem-button");
+        insSem2.click();
+        //setting course pool to add courses to Fall 2022
+        userEvent.selectOptions(termSelect2, "Fall");
+        //adding courses to Fall 2022
+        userEvent.type(inputBox2, "CISC220");
+        addCourse.click();
+        userEvent.clear(inputBox2);
     });
     test("The website is displaying", () => {
         // Up to you to decide what your tests are!
